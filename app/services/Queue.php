8b2e5cbe458e7de1d3ea5099c77910f9c1ac4086
Joe <joe@xxtime.com>
2017-09-30 10:51:39 +0800

    RabbitMQ

--- app/services/Queue.php
+++ app/services/Queue.php
@@ -29,40 +29,72 @@ class Queue
         if (!$messageBody) {
             return false;
         }
-        $config = DI::getDefault()->get('config')->mq;
-        $exchange = 'router'; // direct类型默认交换机
-        $connection = new AMQPStreamConnection($config->host, $config->port, $config->username, $config->password,
-            $config->vhost);
+        $cfg = DI::getDefault()->get('config')->mq;
+        $exchange = 'router';   // direct类型默认交换机
+        $connection = new AMQPStreamConnection($cfg->host, $cfg->port, $cfg->user, $cfg->pass, $cfg->vhost);
         $channel = $connection->channel();
 
 
         /**
+         * 声明一个队列
          * name: $queue
          * passive: false
-         * durable: true // the queue will survive server restarts
-         * exclusive: false // the queue can be accessed in other channels
-         * auto_delete: false //the queue won't be deleted once the channel is closed.
+         * durable: true        // the queue will survive server restarts
+         * exclusive: false     // the queue can be accessed in other channels
+         * auto_delete: false   //the queue won't be deleted once the channel is closed.
          */
         $channel->queue_declare($queue, false, true, false, false);
 
 
         /**
+         * 声明一个交换器
          * name: $exchange
          * type: direct
          * passive: false
-         * durable: true // the exchange will survive server restarts
-         * auto_delete: false //the exchange won't be deleted once the channel is closed.
+         * durable: true        // the exchange will survive server restarts
+         * auto_delete: false   //the exchange won't be deleted once the channel is closed.
          */
         $channel->exchange_declare($exchange, 'direct', false, true, false);
+
+
+        /**
+         * 绑定队列到交换器
+         * @param string $queue
+         * @param string $exchange
+         * @param string $routing_key
+         * @param bool $nowait
+         * @param array $arguments
+         * @param int $ticket
+         * @return mixed|null
+         */
         $channel->queue_bind($queue, $exchange);
+
+
+        // 准备发布消息
         if (!is_array($messageBody)) {
             $messageBody = [$messageBody];
         }
         foreach ($messageBody as $msg) {
-            $message = new AMQPMessage($msg,
-                array('content_type' => 'text/plain', 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
+            // 定义消息
+            $message = new AMQPMessage(
+                $msg,
+                array(
+                    'content_type'  => 'text/plain',
+                    'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT
+                )
+            );
+            /**
+             * 发布消息
+             * @param AMQPMessage $msg
+             * @param string $exchange
+             * @param string $routing_key
+             * @param bool $mandatory
+             * @param bool $immediate
+             * @param int $ticket
+             */
             $channel->basic_publish($message, $exchange);
         }
+
         $channel->close();
         $connection->close();
         return true;
