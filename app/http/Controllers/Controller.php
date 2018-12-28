be717a1461e55cd9633218651687105afc2d8dd4
Joe <joe@xxtime.com>
2018-12-28 17:17:38 +0800

    property

--- app/http/Controllers/Controller.php
+++ app/http/Controllers/Controller.php
@@ -8,6 +8,11 @@ use Phalcon\Mvc\Controller as BaseController;
 use Phalcon\Mvc\Dispatcher;
 
 /**
+ * Class Controller
+ * @package App\Http\Controllers
+ * @property \Phalcon\Config $config
+ * @property \Redis $redis
+ * @property \MongoDB\Client $mongodb
  * @property \App\Providers\System\Locale $locale
  */
 class Controller extends BaseController
--- app/http/Models/Model.php
+++ app/http/Models/Model.php
@@ -5,7 +5,11 @@ namespace App\Http\Models;
 
 use Phalcon\Mvc\Model as BaseModel;
 
-
+/**
+ * Class Model
+ * @package App\Http\Models
+ * @property \Phalcon\DI\FactoryDefault $di
+ */
 class Model extends BaseModel
 {
 
